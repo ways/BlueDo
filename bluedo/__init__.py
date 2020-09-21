1803fbba4ba1e1aa660c715758e512c93269008d
Lars Falk-Petersen <dev@falkp.no>
2020-09-21 11:02:32 +0200

    Only run 'away' once.

--- bluedo/__init__.py
+++ bluedo/__init__.py
@@ -365,7 +365,7 @@ class Application(Gtk.Application):
                     print("L", end='')
 
                 lost_pings += 1
-                if lost_pings >= self.away_count:
+                if lost_pings == self.away_count:
                     #lost_pings = 0
                     away_callback()
             elif lost_pings > 0:
