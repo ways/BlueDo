a1134db7fa514e6d938282e7b20333eff938e838
Lars Falk-Petersen <dev@falkp.no>
2020-09-29 20:32:23 +0200

    Import correct module

--- bluedo/__init__.py
+++ bluedo/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import sys
-import bluedoapp
+from . import bluedoapp
 
 def main(args=None):
     app = bluedoapp.BlueDo()
--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='bluedo',
-    version='0.39',
+    version='0.40',
     description='Bluetooth proximity automation',
     long_description=long_description,
     long_description_content_type="text/markdown",
